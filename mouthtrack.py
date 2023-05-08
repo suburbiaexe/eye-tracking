afc111b05772b2ca39dba030c412d01a74f3f335
isabelleemma <84509710+isabelleemma@users.noreply.github.com>
2023-05-08 16:27:46 -0400

    added some documentation

--- mouthtrack.py
+++ mouthtrack.py
@@ -9,20 +9,39 @@ import dlib
 import cv2
 import pyautogui
 
+#thresholds for marking facial expressions
 MOUTH_AR_THRESH = 0.79
 SCALE_FACTOR = 3.0
-def mouth_aspect_ratio(mouth):
-	A = dist.euclidean(mouth[2], mouth[10]) # 51, 59
-	B = dist.euclidean(mouth[4], mouth[8]) # 53, 57
-	C = dist.euclidean(mouth[0], mouth[6]) # 49, 55
-	return (A + B) / (2.0 * C)
-
 EYE_AR_THRESH = 0.2
+
+def mouth_aspect_ratio(mouth):
+    """calculates the aspect ratio of the mouth
+
+    Args:
+        mouth (list): list of coordinates for points defining the mouth
+
+    Returns:
+        float: mouth aspect ratio
+    """
+    A = dist.euclidean(mouth[2], mouth[10]) # 51, 59
+    B = dist.euclidean(mouth[4], mouth[8]) # 53, 57
+    C = dist.euclidean(mouth[0], mouth[6]) # 49, 55
+    print((A + B) / (2.0 * C))
+    return (A + B) / (2.0 * C)
+
 def eye_aspect_ratio(eye):
-	A = dist.euclidean(eye[1], eye[5]) # 44, 48; one top/bottom pair
-	B = dist.euclidean(eye[2], eye[4]) # 53, 57; other top-bottom pair
-	C = dist.euclidean(eye[0], eye[3]) # 49, 55; left-right extremes
-	return (A + B) / (2.0 * C)
+    """calculates the aspect ratio of the eye
+
+    Args:
+        eye (list): list of coordinates for points defining the eye
+
+    Returns:
+        float: eye aspect ratio
+    """
+    A = dist.euclidean(eye[1], eye[5]) # 44, 48; one top/bottom pair
+    B = dist.euclidean(eye[2], eye[4]) # 53, 57; other top-bottom pair
+    C = dist.euclidean(eye[0], eye[3]) # 49, 55; left-right extremes
+    return (A + B) / (2.0 * C)
 
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
@@ -35,7 +54,7 @@ nose_i = 30 # nose
 
 # start the video stream thread
 print("[INFO] starting video stream thread...")
-vs = VideoStream(src=1).start() #NOTE- src=0 for webcam, 1 for ext camera
+vs = VideoStream(src=0).start() #NOTE- src=0 for webcam, 1 for ext camera
 time.sleep(1.0)
 
 frame_width = 640
@@ -93,7 +112,6 @@ while True:
             pyautogui.mouseUp()
             mouth_open = False
         elif (lear < EYE_AR_THRESH):
-            print(lear)
             pyautogui.click(button='right')
         pyautogui.moveRel(SCALE_FACTOR*-delta_x, SCALE_FACTOR*delta_y)
         
