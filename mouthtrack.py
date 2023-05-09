80315245074f6117a92a6780de743f86c30bbc52
isabelleemma <84509710+isabelleemma@users.noreply.github.com>
2023-05-08 23:00:14 -0400

    more cleanup

--- mouthtrack.py
+++ mouthtrack.py
@@ -77,54 +77,55 @@ while True:
     frame = vs.read()
     frame = imutils.resize(frame, width=frame_width)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ### replaced below w/
     rects = detector(gray, 0)
+    for rect in rects: #TODO- i feel like we should take this out since we only want functionality for 1 face?
+    # face = detector(gray, 0)[0]
+    ###
+        shape = face_utils.shape_to_np(predictor(gray, rect))
 
-    # for rect in rects: #TODO- i feel like we should take this out since we only want functionality for 1 face?
-    shape = predictor(gray, rect)
-    shape = face_utils.shape_to_np(shape)
+        mouth = shape[mStart:mEnd]
+        eyes = shape[36:48]
+        left_eye, right_eye = shape[42:48], shape[36:42]
+        nose = shape[nose_i]
 
-    mouth = shape[mStart:mEnd]
-    eyes = shape[36:48]
-    left_eye, right_eye = shape[42:48], shape[36:42]
-    nose = shape[nose_i]
+        mouthMAR = mouth_aspect_ratio(mouth)
+        mar = mouthMAR
+        mouthHull = cv2.convexHull(mouth)
+        cv2.circle(frame, nose, 2, (0, 0, 255), -1)
+        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+        if mar > MOUTH_AR_THRESH:
+            cv2.putText(frame, ":O", (30,30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255),2)
 
-    mouthMAR = mouth_aspect_ratio(mouth)
-    mar = mouthMAR
-    mouthHull = cv2.convexHull(mouth)
-    cv2.circle(frame, nose, 2, (0, 0, 255), -1)
-    cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
-    if mar > MOUTH_AR_THRESH:
-        cv2.putText(frame, ":O", (30,30),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255),2)
+        if first_loop:
+            prev_x, prev_y = nose
+            first_loop = False
+        delta_x, delta_y = nose[0] - prev_x, nose[1] - prev_y
+        prev_x, prev_y = nose[0], nose[1]
 
-    if first_loop:
-        prev_x, prev_y = nose
-        first_loop = False
-    delta_x, delta_y = nose[0] - prev_x, nose[1] - prev_y
-    prev_x, prev_y = nose[0], nose[1]
+        # compute the convex hull for the mouth, then
+        # visualize the mouth
+        mouthHull = cv2.convexHull(mouth)
+        cv2.circle(frame, nose, 2, (0, 0, 255), -1)
+        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
 
-    # compute the convex hull for the mouth, then
-    # visualize the mouth
-    mouthHull = cv2.convexHull(mouth)
-    cv2.circle(frame, nose, 2, (0, 0, 255), -1)
-    cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+        for p in eyes:
+            cv2.circle(frame, p, 2, (255, 0, 0), -1)
 
-    for p in eyes:
-        cv2.circle(frame, p, 2, (255, 0, 0), -1)
-
-    lear = eye_aspect_ratio(left_eye)
-    if mar > MOUTH_AR_THRESH:
-        cv2.putText(frame, ":O", (30,30),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255),2)
-        if not mouth_open and not DEMO_VIDEO_MODE: pyautogui.mouseDown() 
-        mouth_open = True
-    elif mouth_open and not DEMO_VIDEO_MODE:
-        pyautogui.mouseUp()
-        mouth_open = False
-    elif (lear < EYE_AR_THRESH) and not DRAW_MODE and not DEMO_VIDEO_MODE:
-        pyautogui.click(button='right')
-    if not DEMO_VIDEO_MODE:
-        pyautogui.moveRel(SCALE_FACTOR*-delta_x, SCALE_FACTOR*delta_y)
+        lear = eye_aspect_ratio(left_eye)
+        if mar > MOUTH_AR_THRESH:
+            cv2.putText(frame, ":O", (30,30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255),2)
+            if not mouth_open and not DEMO_VIDEO_MODE: pyautogui.mouseDown() 
+            mouth_open = True
+        elif mouth_open and not DEMO_VIDEO_MODE:
+            pyautogui.mouseUp()
+            mouth_open = False
+        elif (lear < EYE_AR_THRESH) and not DRAW_MODE and not DEMO_VIDEO_MODE:
+            pyautogui.click(button='right')
+        if not DEMO_VIDEO_MODE:
+            pyautogui.moveRel(SCALE_FACTOR*-delta_x, SCALE_FACTOR*delta_y)
         
 
     cv2.imshow("Frame", frame)
