831659cb2f5ab9a7e35bad5d4a5eea2f349021cb
Elizabeth <elizabeth_c_wu@brown.edu>
2023-05-08 22:58:35 -0400

    demo

---
+++ demo/bruh.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    btn = document.getElementById("btn");
+
+    btn.onclick = function () {
+        alert("you clicked!");
+    };
+});
